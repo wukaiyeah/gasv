7bdf6fbab033d668e278e37b7b967f060f70f1b8
tony.cannistra <tony.cannistra@gmail.com>
2012-07-06 12:57:43 +0000

    Added GASVProHQ script, renamed GASVPro-CC

---
+++ GASVPro-HQ.sh
@@ -0,0 +1,118 @@
+#!/bin/sh
+
+#### GASVPro High Quality #####
+
+###############################
+##### SET PARAMETERS HERE #####
+###############################
+
+#REQUIRED:
+BAMFILE=example.bam                     
+
+#OPTIONAL (set to NULL if not being used):
+UNIQUEFILE=/home/tonyc/Desktop/tester
+MAXUNIQUEVAL=37                 #MUST SPECIFY IF UNIQUEFILE IS GIVEN
+MINSCALEDUNIQUE=.3              #MUST SPECIFY IF UNIQUEFILE IS GIVEN
+LRTHRESHOLD=NULL                #default 0
+MINCLUSTER=NULL                 #default 4
+
+
+###############################
+#DO NOT MODIFY BELOW THIS POINT
+###############################
+
+### Check Input ###
+
+echo "\n** GASVProHQ Running... **\n" 
+echo "====Input===="
+if [ -r $BAMFILE ]; then
+    echo "      Valid .bam file: $BAMFILE"
+else
+    echo "      !! WARNING: Cannot open .BAM FILE. Aborting.\n"
+    exit 1
+fi
+if [ -r $UNIQUEFILE ]; then
+    echo "      Unique file: $UNIQUEFILE"
+else
+    echo "      No unique file Provided."
+fi
+
+if [ "$MAXUNIQUEVAL" != "NULL" -a "$MINSCALEDUNIQUE" != "NULL" ]; then
+    echo "          Max Uniqueness Value: $MAXUNIQUEVAL"
+    echo "          Min Scaled Uniqueness: $MINSCALEDUNIQUE"
+else
+    echo "      !! ERROR: Max uniqueness value and minimum scaled uniqueness value must be provided. Please edit your parameters and restart.\n"
+    exit 1
+fi
+
+if [ "$LRTHRESHOLD" = "NULL" ]; then
+    echo "      No LR Threshold Provided"
+else
+    echo "      LR Threshold: " $LRTHRESHOLD
+fi
+if [ "$MINCLUSTER" = "NULL" ]; then
+    echo "      No Min Cluster Size provided"
+else
+    echo "      Min Cluster Size: " $MINCLUSTER
+fi
+echo "=============\n"
+
+echo $S1 $S2
+
+
+
+### Run BAMtoGASV ###
+
+echo "===================================\n\n *** Running BAMToGASV....*** \n\n===================================\n"
+java -jar bin/BAMToGASV.jar $BAMFILE -GASVPRO true 
+
+### Run GASV ###
+
+if [ -r $BAMFILE.gasv.in ]; then
+    echo "====================\n\n *** Running GASV....*** \n\n===================="
+else
+    echo "\n\n!! ERROR: necessary parameters file \"$BAMFILE.gasv.in\" does not exist. Ensure BAMToGASV ran correctly and restart.\n"
+    exit 1
+fi
+
+if [ "$MINCLUSTER" = "NULL" ]; then
+    java -jar bin/GASV.jar --output regions --maximal --batch $BAMFILE.gasv.in
+else
+    java -jar bin/GASV.jar --output regions --maximal --minClusterSize $MINCLUSTER --batch $BAMFILE.gasv.in
+fi
+
+### Run GASV-CC ###
+
+if [ -r $BAMFILE.gasvpro.in ]; then
+    echo "====================\n\n *** Running GASV-CC with the following parameters...*** \n\n"
+else
+    echo "\n\n!! ERROR: necessary parameters file \"$BAMFILE.gasvpro.in\" does not exist. Ensure BAMToGASV ran correctly and restart.\n"
+    exit 1
+fi
+
+if [ -r $UNIQUEFILE ]; then
+    echo "UNIQUEFile: $UNIQUEFILE" >> $BAMFILE.gasvpro.in
+    echo "MaxUniqueValue: $MAXUNIQUEVAL" >> $BAMFILE.gasvpro.in
+    echo "MinScaledUniqueness: $MINSCALEDUNIQUE" >> $BAMFILE.gasvpro.in
+fi
+
+if [ "$LRTHRESHOLD" != "NULL" ]; then
+    echo "LRThreshold: $LRTHRESHOLD" >> $BAMFILE.gasvpro.in
+fi
+
+cat $BAMFILE.gasvpro.in
+echo "/n====================\n\n"
+
+bin/GASVPro-CC $BAMFILE.gasvpro.in $BAMFILE.gasv.in.clusters
+
+
+echo "===================\n\n GASVPro-HQ Completed Successfully \n\n===================\n"
+
+
+
+
+
+
+
+
+
